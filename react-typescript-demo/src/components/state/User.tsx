595d38d9582a752737d9cba4a8521de1ff6d0a43
RashmiHG90 <rashmi.hguk@gmail.com>
2026-03-20 07:35:33 +0000

    handling state and it's future value in typescript

---
+++ react-typescript-demo/src/components/state/User.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+    type AuthUser ={
+        name: string
+        email: string
+    }
+
+export const User = ()=>{
+    const [user, setUser] = useState<AuthUser | null>(null)
+    const handleLogin = () =>{
+        setUser({
+            name: 'Sunshine',
+            email: 'sunshine@gmail.com'
+        })
+    }
+    const handleLogout = ()=>{
+        setUser(null)
+    }
+    return(
+        <>
+        <button onClick = {handleLogin}>Login</button>
+        <button onClick={handleLogout}>Logout</button>
+        <div>User name is {user?.name}</div>
+        <div>User email is{user?.email}</div>
+        </>
+    )
+}
